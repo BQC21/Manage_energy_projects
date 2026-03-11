ab8c142eabd90f09189f8d807864a93a7485c437
Braulio <braulio.quine@utec.edu.pe>
2026-03-11 16:29:32 -0500

    add home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar";
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Welcome to the project manager!</h1>
+            <h3>Manage your PV solar system project here 👇 </h3>
+        </div>
+    );
+}
+
+export default Home;
