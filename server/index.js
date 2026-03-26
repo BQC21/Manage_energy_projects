66eafabb7b81110be2aeca4d4cd6fe4b9bc85cc5
Braulio <braulio.quine@utec.edu.pe>
2026-03-26 14:57:55 -0500

    add express-session packages on ./server/index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import session from "express-session";
 
 import projectRoutes from "./routes/projects.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
+import { isAuthenticated } from "./middlewares/auth.middleware.js";
 import { logger } from "./middlewares/logger.middleware.js";
 import { successHandler } from "./middlewares/successHandler.middleware.js";
 import { notFound } from "./middlewares/notFound.middleware.js";
@@ -11,22 +14,41 @@ import { errorHandler } from "./middlewares/errorHandler.middleware.js";
 
 const app = express();
 
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'tu_clave_secreta_aqui', // Usa .env
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: false, // Ponlo en true solo si usas HTTPS
+        httpOnly: true, 
+        maxAge: 24 * 60 * 60 * 1000 // 1 día de duración
+    }
+}))
+
 // Port
 const port = process.env.PORT || 3000;
 
-// global middlewares 
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.use(express.json());
+
+// global middlewares 
 app.use(logger);          
 app.use(successHandler);              
+app.use(isAuthenticated);
 
-// routes
-app.use("/api", projectRoutes);                
+// RUTAS
+
+// 1. Rutas Públicas (Login, Register, etc.)
+app.use("/api/auth", authRoutes);
+
+// Aplicamos el middleware solo a estas rutas
+app.use("/api", isAuthenticated, projectRoutes);
 
 // middlewares for error management
 app.use(notFound);       
 app.use(errorHandler);    
 
 // start server
-app.listen(port);
-console.log(`server on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server on port ${port}`);
+});
