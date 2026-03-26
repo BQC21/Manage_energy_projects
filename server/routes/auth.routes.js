a1ce5a1050f2ff56ecfb170073ec02cc9d488c17
Braulio <braulio.quine@utec.edu.pe>
2026-03-26 14:18:52 -0500

    add auth routes on Express

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { register, login, logout, validation, update_password } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/validation", validation);
+router.post("/update_password", update_password);
+
+export default router;
