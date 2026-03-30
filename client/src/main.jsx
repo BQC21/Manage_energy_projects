686ee723488e1c55518fcfd176fa4ea713263cae
Braulio <braulio.quine@utec.edu.pe>
2026-03-30 16:03:57 -0500

    split index.css in component design (navbar, graphs and auth)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./styles/index.css";
+import "./styles/navbar.css";
+import "./styles/auth.css";
+import "./styles/graphs.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
